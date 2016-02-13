6ae171e9876d1959a65b874420158cf1e5d0c68f
agentronin316 <agentronin316@gmail.com>
2016-02-13 07:37:15 -0600

    Vector Class documented
    yay.

--- MPGLabs/MPGLabs/Vector3D.cs
+++ MPGLabs/MPGLabs/Vector3D.cs
@@ -17,7 +17,7 @@ namespace MPGLabs
         public float Z { get; private set; }
         public float W { get; private set; }
         
-        //Precalculated conversion values
+        //Precalculate conversion values
         const float DEG_TO_RAD = (float)(180 / Math.PI);
         const float RAD_TO_DEG = (float)(Math.PI / 180);
 
@@ -92,41 +92,57 @@ namespace MPGLabs
 
         }
 
+        /// <summary>
+        /// string will be formatted with angle braces and x, y, z as "0.00, 0.00, 0.00"
+        /// </summary>
+        /// <returns>a string representing the rectangular coordinates of the vector</returns>
         public string PrintRect()
         {
             return String.Format("<{0:N2}, {1:N2}, {2:N2}>", X, Y, Z);
         }
 
+        /// <summary>
+        /// string will be formatted: "0.00 at heading 0.00 and pitch 0.00"
+        /// </summary>
+        /// <returns>a string containing the magnitude, heading, and pitch of the vector with angles in degrees</returns>
         public string PrintMagHeadPitch()
         {
             return String.Format("{0:N2} at heading {1:N2} and pitch {2:N2}", GetMagnitude(), GetHeading(), GetPitch());
         }
 
+        /// <summary>
+        /// string will be formatted: "alpha = 0.00; beta = 0.00; gamma = 0.00"
+        /// </summary>
+        /// <returns>a string containing all three Euler Angles of the vector in degrees</returns>
         public string PrintEulerAngles()
         {
             return String.Format("alpha = {0:N2}; beta = {1:N2}; gamma = {2:N2}", GetAlpha(), GetBeta(), GetGamma());
         }
 
+        /// <summary>
+        /// Calculates the magnitude of the vector
+        /// </summary>
+        /// <returns>the magnitude of the vector</returns>
         public float GetMagnitude()
         {
-            return (float)Math.Sqrt(X * X + Y * Y + Z * Z);
+            return (float)Math.Sqrt(X * X + Y * Y + Z * Z); //distance formula
         }
 
         /// <summary>
-        /// 
+        /// Returns the angle between the positive x-axis and the vector in the xy-plane
         /// </summary>
         /// <returns>The heading of the vector in degrees</returns>
         public float GetHeading()
         {
             if (X == 0)
             {
-                if (Y > 0)
+                if (Y > 0) //vector points straight up
                 {
                     return 90f;
                 }
                 else if (Y < 0)
                 {
-                    return -90f;
+                    return -90f; //vector points straight down
                 }
                 else //Y == 0
                 {
@@ -135,7 +151,7 @@ namespace MPGLabs
             }
             else if (X > 0)
             {
-                if (Y == 0)
+                if (Y == 0) //shortcut the function call
                 {
                     return 0f;
                 }
@@ -144,9 +160,9 @@ namespace MPGLabs
                     return Rad2Deg((float)Math.Atan(Y / X));
                 }
             }
-            else // x < 0
+            else // x < 0, need to add 180 degrees to get the correct quadrant
             {
-                if (Y == 0)
+                if (Y == 0) //shortcut the function call
                 {
                     return 180f;
                 }
@@ -159,22 +175,38 @@ namespace MPGLabs
             
         }
 
+        /// <summary>
+        /// Returns the angle between the vector and the xy-plane
+        /// </summary>
+        /// <returns>The pitch of the vector in degrees</returns>
         public float GetPitch()
         {
             return Rad2Deg((float)Math.Asin(Z));
         }
 
-        public float GetAlpha() //Euler Angle to x-axis in degrees
+        /// <summary>
+        /// Returns the Euler Angle to x-axis
+        /// </summary>
+        /// <returns>Alpha Euler Angle in degrees</returns>
+        public float GetAlpha() 
         {
             return Rad2Deg((float)(Math.Acos(X / GetMagnitude())));
         }
 
-        public float GetBeta() //Euler Angle to y-axis in degrees
+        /// <summary>
+        /// Returns the Euler Angle to y-axis
+        /// </summary>
+        /// <returns>Beta Euler Angle in degrees</returns>
+        public float GetBeta()
         {
             return Rad2Deg((float)(Math.Acos(Y / GetMagnitude())));
         }
 
-        public float GetGamma() //Euler Angle to z-axis in degrees
+        /// <summary>
+        /// Returns the Euler Angle to z-axis
+        /// </summary>
+        /// <returns>Gamma Euler Angle in degrees</returns>
+        public float GetGamma()
         {
             return Rad2Deg((float)(Math.Acos(Z / GetMagnitude())));
         }
